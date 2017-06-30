7273293db9191dad44dc6f36ffee843f25051373
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2017-06-30 14:46:14 +0300

    complete code for webinar

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,26 @@
-(function() {
+//parallax
+(function () {
+  var parallaxContainer = document.getElementById('parallax'),
+    layers = parallaxContainer.children;
 
+  var moveLayers = function (e) {
+    var 
+      initialX = (window.innerWidth / 2) - e.pageX,
+      initialY = (window.innerHeight / 2) - e.pageY;
+
+    [].slice.call(layers).forEach(function (layer, i) {
+      var
+        divider = i / 100,
+        positionX = initialX * divider,
+        positionY = initialY * divider,
+        bottomPosition = (window.innerHeight / 2) * divider,
+        transformString = 'translate(' + positionX + 'px ,' + positionY + 'px)',
+        image = layer.firstElementChild;
+
+      layer.style.transform = transformString;
+      image.style.bottom = '-' + bottomPosition + 'px';
+    });
+  };
+
+  window.addEventListener('mousemove', moveLayers);
 })();
