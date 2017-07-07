e0d69abbf2154ff14c99bc94795cc26915ee6a6e
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2017-07-07 17:31:42 +0300

    prepare for webinar

--- source/js/app.js
+++
@@ -1,9 +0,0 @@
-import preloader from './modules/preloader'
-import slider from './modules/slider'
-import slideshow from './modules/slideshow'
-
-$(document).ready(() => {
-  preloader();
-  slider();
-  slideshow();
-});
